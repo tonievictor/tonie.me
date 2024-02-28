aa3a3afaa53eaa36c1973d76920668acb75e44dc
Tonie Victor <victornwanochi@gmail.com>
2024-02-28 17:59:50 +0100

    feat: implemented the bulk of the project. Mostly dummy data and images for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import icon from "astro-icon";
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  site: 'https://example.com',
+  integrations: [icon(), mdx(), sitemap()]
 });
---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import {defineCollection, z} from 'astro:content';
+
+const articles = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.string().transform((str) => new Date(str)),
+        tags: z.string().optional(),
+        draft: z.boolean().default(false),
+		heroImage: z.string().optional(),
+	}),
+});
+
+export const collections = { articles };
