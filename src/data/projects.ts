79b96a674a391a1b46038225789242e707a0f8e1
Tonie Victor <victornwanochi@gmail.com>
2024-02-29 16:21:24 +0100

    refactor: seo and projects

---
+++ src/data/projects.ts
@@ -0,0 +1,71 @@
+export type ProjectsDataType = {
+  id: number;
+  title: string;
+  overview: string;
+  githublink?: string;
+  image: string;
+  imagealt: string;
+  websitelink?: string;
+};
+
+export const ProjectsData: ProjectsDataType[] = [
+  {
+    id: 0,
+    title: "Go dotenv",
+    overview:
+      "lsnjsbs s hsvh shbvsjs hbshs  hsh jbsji iajm n sjbisnjbnsu ubnjs jbib biu busi i",
+    githublink: "ksmskms",
+    image: "/images/projectsample.png",
+    websitelink: "ksns",
+    imagealt: "",
+  },
+  {
+    id: 1,
+    title: "Go dotenv",
+    overview:
+      "lsnjsbs s hsvh shbvsjs hbshs  hsh jbsji iajm n sjbisnjbnsu ubnjs jbib biu busi i",
+    githublink: "ksnkns",
+    image: "/images/projectsample.png",
+
+    imagealt: "",
+  },
+  {
+    id: 2,
+    title: "Go dotenv",
+    overview:
+      "lsnjsbs s hsvh shbvsjs hbshs  hsh jbsji iajm n sjbisnjbnsu ubnjs jbib biu busi i",
+
+    image: "/images/projectsample.png",
+    websitelink: "jsbs",
+    imagealt: " jsnsjs",
+  },
+  {
+    id: 3,
+    title: "Go dotenv",
+    overview:
+      "lsnjsbs s hsvh shbvsjs hbshs  hsh jbsji iajm n sjbisnjbnsu ubnjs jbib biu busi i",
+    githublink: "sknsjksn",
+    image: "/images/projectsample.png",
+    imagealt: "",
+  },
+  {
+    id: 4,
+    title: "Go dotenv",
+    overview:
+      "lsnjsbs s hsvh shbvsjs hbshs  hsh jbsji iajm n sjbisnjbnsu ubnjs jbib biu busi i",
+    githublink: "",
+    image: "/images/projectsample.png",
+    websitelink: "jsnsj",
+    imagealt: "",
+  },
+  {
+    id: 5,
+    title: "Go dotenv",
+    overview:
+      "lsnjsbs s hsvh shbvsjs hbshs  hsh jbsji iajm n sjbisnjbnsu ubnjs jbib biu busi i",
+    githublink: "ksnsk",
+    image: "/images/projectsample.png",
+    websitelink: "jsnjs",
+    imagealt: "",
+  },
+];
