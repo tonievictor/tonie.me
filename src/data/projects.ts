db915b764dcd796e625ae83edbcc161bec500a19
Tonie Victor <victornwanochi@gmail.com>
2024-08-17 19:04:41 +0100

    chore: updated social profile links (github and twitter)

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -11,21 +11,21 @@ export const ProjectsData: ProjectsDataType[] = [
     id: 0,
     title: "Simple shell",
     overview: "An implementation of the UNIX shell program.",
-    link: "https://github.com/tonie-ng/simple_shell",
+    link: "https://github.com/tonievictor/simple_shell",
 		tags: ["c", "unix", "shell"]
   },
   {
     id: 1,
     title: "Go dotenv",
     overview: "A dotenv package for go projects.",
-    link: "https://github.com/tonie-ng/go-dotenv",
+    link: "https://github.com/tonievictor/go-dotenv",
 		tags: ["golang", "dotenv"]
   },
   {
     id: 2,
     title: "Socket GC",
     overview: "A terminal based groupchat application.",
-    link: "https://github.com/tonie-ng/socket-gc",
+    link: "https://github.com/tonievictor/socket-gc",
 		tags: ["c", "networking", "sockets"]
   },
 ];
