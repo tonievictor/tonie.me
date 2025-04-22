15b08bcecf4bea961fb22cd27ca85492c2f08a3b
Tonie Victor <victornwanochi@gmail.com>
2025-04-22 09:16:32 +0100

    update projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,31 +1,38 @@
 export type ProjectsDataType = {
-  id: number;
-  title: string;
-  overview: string;
-  link?: string;
+	id: number;
+	title: string;
+	overview: string;
+	link?: string;
 	tags?: string[];
 };
 
 export const ProjectsData: ProjectsDataType[] = [
-  {
-    id: 0,
-    title: "Simple shell",
-    overview: "An implementation of the UNIX shell program.",
-    link: "https://github.com/tonievictor/simple_shell",
-		tags: ["c", "unix", "shell"]
-  },
-  {
-    id: 1,
-    title: "Go dotenv",
-    overview: "A dotenv package for go projects.",
-    link: "https://github.com/tonievictor/go-dotenv",
+	{
+		id: 0,
+		title: "Rexen",
+		overview: "Compile and evaluate regular expressions using Non-deterministic Finite Automata (NFAs).",
+		link: "https://hexdocs.pm/rexen",
+		tags: ["gleam", "regex"]
+	},
+	{
+		id: 1,
+		title: "Dotenv",
+		overview: "A package for managing environment variables.",
+		link: "https://pkg.go.dev/github.com/tonievictor/dotenv",
 		tags: ["golang", "dotenv"]
-  },
-  {
-    id: 2,
-    title: "Socket GC",
-    overview: "A terminal based groupchat application.",
-    link: "https://github.com/tonievictor/socket-gc",
+	},
+	{
+		id: 2,
+		title: "Tsh",
+		overview: "An implementation of the UNIX shell program.",
+		link: "https://github.com/tonievictor/tsh",
+		tags: ["c", "unix", "shell"]
+	},
+	{
+		id: 3,
+		title: "Socket GC",
+		overview: "A terminal based groupchat application.",
+		link: "https://github.com/tonievictor/socket-gc",
 		tags: ["c", "networking", "sockets"]
-  },
+	},
 ];
